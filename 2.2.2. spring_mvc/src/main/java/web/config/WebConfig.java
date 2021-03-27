2640bbf56d1316ed8b0bb53664b3c87d50a774d4
plsnogod <plsnogood@mail.ru>
2021-03-27 13:42:13 +0300

    Cars

--- 2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
+++ 2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
@@ -10,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import web.model.Car;
+import web.service.HangarService;
+import web.service.HangarServiceImpl;
 
 @Configuration
 @EnableWebMvc
@@ -39,8 +42,16 @@ public class WebConfig implements WebMvcConfigurer {
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
     }
-
-
+    @Bean
+    public HangarService createHangar(){
+        HangarService hangar = new HangarServiceImpl();
+        hangar.addCar(new Car("Russia","Lada","1980"));
+        hangar.addCar(new Car("USSR","UAZ","1960"));
+        hangar.addCar(new Car("Germany","Mercedes","2001"));
+        hangar.addCar(new Car("Japan","Furio","2005"));
+        hangar.addCar(new Car("USA","Corvett","2019"));
+        return hangar;
+    }
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
